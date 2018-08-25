097ae41de056a4ea09cd911d91d13a76fccb4474
Robert Peteuil <robert@peteuil.com>
2018-08-24 20:48:31 -0700

    first commit

---
+++ shell/grc/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+PREFIX=${1:-/usr/local}
+ETCPREFIX=$2
+
+
+BINDIR=$PREFIX/bin
+LIBDIR=$PREFIX/share/grc
+MANDIR=$PREFIX/share/man
+CONFDIR=$ETCPREFIX/etc
+PROFILEDIR=$CONFDIR/profile.d
+
+mkdir -p $BINDIR || true
+cp -fv grc grcat $BINDIR
+mkdir -p $LIBDIR || true
+cp -fv colourfiles/conf.* $LIBDIR
+mkdir -p $MANDIR/man1
+cp -fv grc.1 $MANDIR/man1
+cp -fv grcat.1 $MANDIR/man1
+mkdir -p $CONFDIR
+cp -fv grc.conf $CONFDIR
+cp -fv grc.zsh $CONFDIR
+cp -fv grc.fish $CONFDIR
+mkdir -p $PROFILEDIR
+cp -fv grc.bashrc $PROFILEDIR
+
