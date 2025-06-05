59e47086e8a9d2f4d238f61c8de82b01cf7fb005
Robert Peteuil <robert@peteuil.com>
2025-06-05 09:43:30 -0700

    feat: cli util bootstrap

---
+++ bootstrap/install-shelll-utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CPU=$(uname -m)
+
+if [[ "$CPU" == "x86_64" ]]; then
+    ARCH="x86_64"
+elif [[ "$CPU" == "aarch64" ]]; then
+    ARCH="aarch64"
+fi
+
+if ! command -v eza &>/dev/null; then
+    echo "eza is not installed. Installing..."
+    file_url="https://github.com/eza-community/eza/releases/latest/download/eza_${ARCH}-unknown-linux-gnu.tar.gz"
+    wget -c "$file_url" -O - | tar xz
+    chmod +x eza
+	sudo mv eza /usr/local/bin/eza
+fi
+
+if ! command -v zoxide &>/dev/null; then
+    echo "zoxide is not installed. Installing..."
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+fi
+
+if ! command -v oh-my-posh &>/dev/null; then
+    echo "oh-my-posh is not installed. Installing..."
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+fi
