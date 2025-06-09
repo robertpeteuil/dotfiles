952a3676b0acdbed768829e27dbd4f9b5798a7fb
Robert Peteuil <robert@peteuil.com>
2025-06-09 14:41:20 -0700

    feat: cross-platform shell util install script

--- bootstrap/install-lazygit.sh
+++ bootstrap/install-lazygit.sh
@@ -5,28 +5,36 @@ OS="$(uname -s)"
 if [[ "$OS" == 'Linux' ]]; then
 
   CPU=$(uname -m)
-
-  echo "CPU: $CPU"
-
   if [[ "$CPU" == "x86_64" ]]; then
-      ARCH="x86_64"
+    ARCH="x86_64"
   elif [[ "$CPU" == "aarch64" ]]; then
-      ARCH="arm64"
+    ARCH="arm64"
   fi
-
   echo "ARCH: $ARCH"
 
   if ! command -v lazygit &>/dev/null; then
-      echo "lazygit is not installed. Installing..."
-      LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    echo "lazygit is not installed. Installing..."
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 
-      echo "LAZYGIT VERSION: $LAZYGIT_VERSION"
+    echo "LAZYGIT VERSION: $LAZYGIT_VERSION"
 
-      curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${ARCH}.tar.gz"
-      tar xf lazygit.tar.gz lazygit
-      # sudo install lazygit -D -t /usr/local/bin/
-      chmod +x lazygit
-      sudo mv lazygit /usr/local/bin/lazygit
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${ARCH}.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    chmod +x lazygit
+    sudo mv lazygit /usr/local/bin/lazygit
   fi
+
+elif [[ "$OS" == 'Darwin' ]]; then
+
+  if ! command -v lazygit &>/dev/null; then
+    if command -v brew &>/dev/null; then
+      echo "lazygit is not installed. Installing..."
+      brew install lazygit
+    else
+      echo "brew missing... lazygit not installed"
+      exit 1
+    fi
+  fi
+
 fi
 
