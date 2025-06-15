2bc3e5f7f77518b4b69abfec5058739915493809
Robert Peteuil <robert@peteuil.com>
2025-06-15 15:18:50 -0700

    feat: nvim add lualine statusbar

---
+++ config/nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        extensions = { 'neo-tree', 'lazy', 'fzf' },
+        globalstatus = true,
+      },
+      inactive_sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
+  end,
+}
