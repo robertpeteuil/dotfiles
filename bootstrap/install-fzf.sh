c1bbda3a5b9fde6fccbb5c6d6b585f4b797fc881
Robert Peteuil <robert@peteuil.com>
2025-06-25 09:40:34 -0700

    fix: ensure script uses real bash

--- bootstrap/install-fzf.sh
+++ bootstrap/install-fzf.sh
@@ -2,7 +2,7 @@
 
 if ! command -v fzf &>/dev/null; then
   echo "fzf is not installed. Installing..."
-  curl -sSfL https://raw.githubusercontent.com/junegunn/fzf/master/install | sh
+  curl -sSfL https://raw.githubusercontent.com/junegunn/fzf/master/install | bash
 else
   echo "fzf already installed."
 fi
