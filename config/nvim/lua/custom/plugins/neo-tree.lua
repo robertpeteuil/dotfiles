57b733f240e3be6da91700e889f6fe84f6905c4f
Robert Peteuil <robert@peteuil.com>
2025-10-02 10:50:00 -0700

    chore: cleanup unused nvim neotree init funct

--- config/nvim/lua/custom/plugins/neo-tree.lua
+++ config/nvim/lua/custom/plugins/neo-tree.lua
@@ -7,21 +7,6 @@ return {
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
-  -- cmd = 'Neotree',
-  -- init = function()
-  --   vim.api.nvim_create_autocmd('BufEnter', {
-  --     -- make a group to be able to delete it later
-  --     group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
-  --     callback = function()
-  --       local f = vim.fn.expand '%:p'
-  --       if vim.fn.isdirectory(f) ~= 0 then
-  --         vim.cmd('Neotree current dir=' .. f)
-  --         -- neo-tree is loaded now, delete the init autocmd
-  --         vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
-  --       end
-  --     end,
-  --   })
-  -- end,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
