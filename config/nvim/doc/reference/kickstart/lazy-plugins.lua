b14f76e3e1917cabe28ac90818c73cae6427ac38
Robert Peteuil <robert@peteuil.com>
2025-11-27 16:24:09 -0700

    refactor(nvim): change plugin dir structure

---
+++ config/nvim/doc/reference/kickstart/lazy-plugins.lua
@@ -0,0 +1,51 @@
+-- [[ Configure and install plugins ]]
+
+require('lazy').setup({
+
+  {
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    opts = {},
+  },
+
+  require 'kickstart.plugins.blink-cmp',
+  require 'kickstart.plugins.conform',
+  require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.lspconfig',
+  require 'kickstart.plugins.todo-comments',
+
+  -- Moved to custom/plugins dir for Customization
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.which-key',
+  -- require 'kickstart.plugins.telescope',
+  -- require 'kickstart.plugins.mini',
+  -- require 'kickstart.plugins.tokyonight',
+  -- require 'kickstart.plugins.treesitter',
+
+  -- Stopped using - moved to reference dir
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.lint',
+
+  { import = 'custom.plugins' },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
