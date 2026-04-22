55c82df95c1c59d445c60a31b11b6d7f43c5b56f
Robert Peteuil <robert@peteuil.com>
2026-04-22 12:38:24 -0700

    fix(nvim): switch to mini.comment from comment.nvim
    - fix incompatibility with nvim 0.12

--- config/nvim/lua/plugins/auto/mini.lua
+++ config/nvim/lua/plugins/auto/mini.lua
@@ -10,6 +10,8 @@ return {
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
+      require('mini.comment').setup()
+
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
