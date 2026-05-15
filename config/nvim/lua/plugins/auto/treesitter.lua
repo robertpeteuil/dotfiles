c3296748013ff4b0d498f21511648997b7e4c4cf
Robert Peteuil <robert@peteuil.com>
2026-05-14 18:00:45 -0700

    fix(nvim): improved treesitter init process
    - treesitter only enabled for specified langs
    - init funct - file opened type is on lang list
      - auto-install language files (if needed)
      - enable treesitter for current buffer
      - enable indentation

--- config/nvim/lua/plugins/auto/treesitter.lua
+++ config/nvim/lua/plugins/auto/treesitter.lua
@@ -4,10 +4,13 @@ local languages = {
   'cpp',
   'css',
   'dockerfile',
+  'fish',
   'go',
+  'gomod',
   'hcl',
   'helm',
   'html',
+  'ini',
   'java',
   'javascript',
   'json',
@@ -15,17 +18,18 @@ local languages = {
   'lua',
   'make',
   'markdown',
+  'markdown_inline',
+  'ninja',
   'python',
-  'query',
-  'regex',
   'rust',
+  'scala',
   'terraform',
   'toml',
-  'tsx',
   'typescript',
+  'vim',
   'vimdoc',
   'yaml',
-  'zig',
+  'zsh',
 }
 
 return {
@@ -34,12 +38,23 @@ return {
   branch = 'main',
   build = ':TSUpdate',
   config = function()
+    local ts_gid = vim.api.nvim_create_augroup('treesitter_fts', { clear = true })
     vim.api.nvim_create_autocmd('FileType', {
+      pattern = languages,
       callback = function()
-        pcall(vim.treesitter.start)
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+        local nvim_ts = require 'nvim-treesitter'
+        local installed_parsers = nvim_ts.get_installed()
+        if not vim.list_contains(installed_parsers, vim.bo.filetype) then
+          nvim_ts.install(vim.bo.filetype):wait(10000)
+        end
+
+        -- enable highlighting with treesitter for config defined 'languages'
+        vim.treesitter.start()
+
+        -- enable indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
+      group = ts_gid,
     })
-    require('nvim-treesitter').install(languages):wait(300000)
   end,
 }
