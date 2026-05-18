af2f788d13b32a475c90ae8cfe265140e1eff577
Robert Peteuil <robert@peteuil.com>
2026-05-18 15:23:17 -0700

    config(nvim): switch git hotkeys to leader-g

---
+++ config/nvim/lua/plugins/auto/gitsigns.lua
@@ -0,0 +1,81 @@
+-- Alternatively, use `config = function() ... end` for full control over the configuration.
+-- If you prefer to call `setup` explicitly, use:
+--    {
+--        'lewis6991/gitsigns.nvim',
+--        config = function()
+--            require('gitsigns').setup({
+--                -- Your gitsigns configuration here
+--            })
+--        end,
+--    }
+--
+-- Here is a more advanced example where we pass configuration
+-- options to `gitsigns.nvim`.
+--
+-- See `:help gitsigns` to understand what the configuration keys do
+return {
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      numhl = true,
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        -- Actions
+        -- visual mode
+        map('v', '<leader>gs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[s]tage hunk' })
+        map('v', '<leader>gr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[r]eset hunk' })
+        -- normal mode
+        map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[s]tage hunk' })
+        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[r]eset hunk' })
+        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[S]tage buffer' })
+        map('n', '<leader>gu', gitsigns.stage_hunk, { desc = '[u]ndo stage hunk' })
+        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[R]eset buffer' })
+        map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[p]review hunk' })
+        map('n', '<leader>gb', gitsigns.blame_line, { desc = '[b]lame line' })
+        map('n', '<leader>gd', gitsigns.diffthis, { desc = '[d]iff against index' })
+        map('n', '<leader>gD', function()
+          gitsigns.diffthis '@'
+        end, { desc = '[D]iff against last commit' })
+        -- Toggles
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+      end,
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
--- config/nvim/lua/plugins/auto/lualine.lua
+++ config/nvim/lua/plugins/auto/lualine.lua
@@ -16,7 +16,7 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff' },
-        lualine_c = { 'filename', 'b:gitsigns_status' },
+        lualine_c = { 'filename' },
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
