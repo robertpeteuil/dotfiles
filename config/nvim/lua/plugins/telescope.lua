13d32fc199cfd42b2e6e3c93d007f9c24f2cf321
Robert Peteuil <robert@peteuil.com>
2026-05-19 15:25:52 -0700

    chore(nvim): set telescope to release versions only

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,7 @@
 return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
+    version = '*',
     event = 'VimEnter',
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -156,7 +157,7 @@ return {
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
+          -- winblend = 10,
           previewer = false,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
