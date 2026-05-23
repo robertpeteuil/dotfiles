bc038a3c7a0d231cfc399b28a155ef2da30ffb02
Robert Peteuil <robert@peteuil.com>
2026-05-23 13:45:47 -0700

    feat(nvim): undotree toggle with neotree inverse-toggle and match width

--- config/nvim/lua/plugins/undotree.lua
+++ config/nvim/lua/plugins/undotree.lua
@@ -1,20 +1,23 @@
 return {
   'mbbill/undotree',
   config = function()
+    vim.g.undotree_SplitWidth = 35
+
     -- Custom Function to auto-hide to toggle undotree and neo-tree simulataneously
     --    toggling ON undotree will auto-hide neo-tree if visible
     --    togglinf OFF undotree will auto-show neo-tree if it was visible
     local neo_tree_was_open = false
+    local neo_tree_width = nil
 
-    local function is_neo_tree_visible()
+    local function get_neo_tree_win()
       for _, win in ipairs(vim.api.nvim_list_wins()) do
         local buf = vim.api.nvim_win_get_buf(win)
         local ft = vim.bo[buf].filetype
         if ft == 'neo-tree' then
-          return true
+          return win
         end
       end
-      return false
+      return nil
     end
 
     local function is_undotree_visible()
@@ -30,26 +33,28 @@ return {
 
     local function toggle_undotree_with_neo_tree()
       if not is_undotree_visible() then
-        -- We want to show undotree
-        if is_neo_tree_visible() then
+        local neo_tree_win = get_neo_tree_win()
+        if neo_tree_win then
           neo_tree_was_open = true
+          neo_tree_width = vim.api.nvim_win_get_width(neo_tree_win)
+          vim.g.undotree_SplitWidth = neo_tree_width + 0
           vim.cmd 'Neotree close'
         else
           neo_tree_was_open = false
+          neo_tree_width = nil
         end
         vim.cmd 'UndotreeShow'
         vim.cmd 'UndotreeFocus'
       else
-        -- Undotree is visible, hide it
         vim.cmd 'UndotreeHide'
         if neo_tree_was_open then
           vim.cmd 'Neotree action=show toggle reveal'
           neo_tree_was_open = false
         end
+        neo_tree_width = nil
       end
     end
 
     vim.keymap.set('n', '<leader>u', toggle_undotree_with_neo_tree, { desc = '[u]ndotree' })
-    -- vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[u]ndotree', silent = true })
   end,
 }
