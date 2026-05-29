cfec8677b5bfdb32b8bd547392d339926b01ed3a
Robert Peteuil <robert@peteuil.com>
2026-05-29 16:41:29 -0700

    chore(bootstrap): clean linter error in install scripts

--- bootstrap/install-omp-manual.sh
+++ bootstrap/install-omp-manual.sh
@@ -6,16 +6,16 @@ executable=""
 version=""
 
 error() {
-  printf "\e[31m$1\e[0m\n"
+  printf '\e[31m%b\e[0m\n' "$1"
   exit 1
 }
 
 info() {
-  printf "$1\n"
+  printf '%b\n' "$1"
 }
 
 warn() {
-  printf "⚠️  \e[33m$1\e[0m\n"
+  printf '⚠️  \e[33m%b\e[0m\n' "$1"
 }
 
 help() {
@@ -53,7 +53,7 @@ done
 SUPPORTED_TARGETS="linux-386 linux-amd64 linux-arm linux-arm64 darwin-amd64 darwin-arm64 freebsd-386 freebsd-amd64 freebsd-arm freebsd-arm64"
 
 validate_dependency() {
-  if ! command -v $1 >/dev/null; then
+  if ! command -v "$1" >/dev/null; then
     error "$1 is required to install Oh My Posh. Please install $1 and try again.\n"
   fi
 }
@@ -76,8 +76,8 @@ set_install_directory() {
   # to install into and follow symlinks
   if command -v oh-my-posh >/dev/null; then
     posh_dir=$(command -v oh-my-posh)
-    real_dir=$(realpath $posh_dir)
-    install_dir=$(dirname $real_dir)
+    real_dir=$(realpath "$posh_dir")
+    install_dir=$(dirname "$real_dir")
     info "Oh My Posh is already installed, updating existing installation in:"
     info "  ${install_dir}"
     return 0
@@ -90,7 +90,7 @@ set_install_directory() {
   fi
 
   # check if $HOME/.local/bin exists and is writable
-  if ([ -d "$HOME/.local/bin" ] && [ -w "$HOME/.local/bin" ]) || mkdir -p "$HOME/.local/bin"; then
+  if { [ -d "$HOME/.local/bin" ] && [ -w "$HOME/.local/bin" ]; } || mkdir -p "$HOME/.local/bin"; then
     install_dir="$HOME/.local/bin"
     return 0
   fi
@@ -144,7 +144,7 @@ install_themes() {
     themes_dir="${themes_dir/#\~/$HOME}"
   fi
 
-  cache_dir=$($executable cache path)
+  cache_dir=$("$executable" cache path)
 
   # validate if the user set the path to the themes directory
   if [ -z "$themes_dir" ]; then
@@ -159,13 +159,13 @@ install_themes() {
 
   url="https://cdn.ohmyposh.dev/releases/latest/themes.zip"
 
-  http_response=$(curl -s -f -L $url -o $zip_file -w "%{http_code}")
+  http_response=$(curl -s -f -L "$url" -o "$zip_file" -w "%{http_code}")
 
-  if [ $http_response = "200" ] && [ -f $zip_file ]; then
-    7z -o -q $zip_file -d $themes_dir
+  if [ "$http_response" = "200" ] && [ -f "$zip_file" ]; then
+    7z -o -q "$zip_file" -d "$themes_dir"
     # make sure the files are readable and writable for all users
-    chmod a+rwX ${themes_dir}/*.omp.*
-    rm $zip_file
+    chmod a+rwX "$themes_dir"/*.omp.*
+    rm "$zip_file"
   else
     warn "Unable to download themes at ${url}\nPlease validate your curl, connection and/or proxy settings"
   fi
@@ -192,7 +192,7 @@ install() {
 
   info "\nℹ️  Installing oh-my-posh for ${target} in ${install_dir}"
 
-  executable=${install_dir}/oh-my-posh
+  executable="${install_dir}/oh-my-posh"
   url=https://cdn.ohmyposh.dev/releases/latest/posh-${target}
   if [ "$version" ]; then
     url=https://cdn.ohmyposh.dev/releases/${version}/posh-${target}
@@ -200,19 +200,19 @@ install() {
 
   info "⬇️  Downloading oh-my-posh from ${url}"
 
-  http_response=$(curl -s -f -L $url -o $executable -w "%{http_code}")
+  http_response=$(curl -s -f -L "$url" -o "$executable" -w "%{http_code}")
 
-  if [ $http_response != "200" ] || [ ! -f $executable ]; then
+  if [ "$http_response" != "200" ] || [ ! -f "$executable" ]; then
     error "Unable to download executable at ${url}\nPlease validate your curl, connection and/or proxy settings"
   fi
 
-  chmod +x $executable
+  chmod +x "$executable"
 
   install_themes
 
   info "🚀 Installation complete.\n\nYou can follow the instructions at https://ohmyposh.dev/docs/installation/prompt"
   info "to setup your shell to use oh-my-posh."
-  if [ $http_response = "200" ]; then
+  if [ "$http_response" = "200" ]; then
     info "\nIf you want to use a built-in theme, you can find them in the ${themes_dir} directory:"
     info "  oh-my-posh init {shell} --config ${themes_dir}/{theme}.omp.json\n"
   fi
@@ -251,3 +251,5 @@ validate_dependencies
 set_install_directory
 validate_install_directory
 install
+
+# vim: ft=bash
